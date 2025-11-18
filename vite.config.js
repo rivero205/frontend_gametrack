c16b54284afe9936170f414acda26a1a6309a66a
Maicol Vivero <111386870+rivero205@users.noreply.github.com>
2025-11-17 22:58:52 -0500

    Remove base URL from Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://rivero205.github.io/frontend_gametrack/',
   plugins: [react()],
 })
